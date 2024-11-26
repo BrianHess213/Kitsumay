39b7212e1667751fde387da3583a1991ff32354d
Brian Hess <109031060+BrianHess213@users.noreply.github.com>
2024-11-26 14:23:30 -0700

    Fixed a bug where images wasn't showing up in my post page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io/image",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
+      {
+        protocol: "https",
+        hostname: "tenor.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media1.tenor.com",
+      },
+      
+    ],
+  },
+
   reactStrictMode: false,
 
 };
