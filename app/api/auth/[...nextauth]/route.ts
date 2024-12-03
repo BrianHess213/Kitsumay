5daf230a16d411ae4658229a220dd9c59fd6c6e9
Brian Hess <109031060+BrianHess213@users.noreply.github.com>
2024-12-03 15:08:10 -0700

    Resolving this probelm

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,24 @@
-import { authOptions } from "@/app/utils/authOptions";
 import NextAuth from "next-auth/next";
+import { NextAuthOptions, SessionStrategy } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import { XataAdapter } from '@next-auth/xata-adapter';
+import { XataClient } from '@/src/xata';
+
+const client = new XataClient();
+
+const authOptions: NextAuthOptions = {
+  adapter: XataAdapter(client),
+  providers: [
+      GitHubProvider({
+          clientId: process.env.GITHUB_ID as string,
+          clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+  ],
+  session: {
+      strategy: "jwt" as SessionStrategy,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 
 export const handler = NextAuth(authOptions);
